afedb74bdf6e6dd2c670ddfea06978fae1d7bf37
walfrido-oliveira <walfrido_15@hotmail.com>
2020-03-20 15:46:47 -0300

    updating files

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -21,4 +21,21 @@
             @if (($key + 1) % 3 == 0) </div><div class="row front">  @endif
         @endforeach
     </div>
+    <div class="row">
+        <div class="col-12">
+            <h2>Lojas Destaque</h2>
+            <hr>
+        </div>
+        @foreach ($stores as $store)
+        <div class="col-4">
+            @if($store->logo)
+                <img src="{{asset('storage/' . $store->logo)}}" alt="Logo da Loja {{$store->name}}" class="img-fluid">
+            @else
+                <img src="https://via.placeholder.com/256X197.png?text=logo" alt="Logo da Loja {{$store->name}}" class="img-fluid">
+            @endif
+            <h3>{{$store->name}}</h3>
+            <p>{{$store->description}}</p>
+        </div>
+        @endforeach
+    </div>
 @endsection
